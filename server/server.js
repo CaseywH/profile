62354598123032ecfd27727c1ff63049e9d5067a
Casey Heinemeyer <cheinemeyer@gmail.com>
2018-03-23 19:34:34 -0700

    heroku

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, "../public");
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+
+
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Spinning up server on port ${port}`);
+});
